276f94aad23f9fe15db3d2b299607c1278f997b0
akmal09 <akmal.119140203@student.itera.ac.id>
2022-07-13 19:31:50 +0600

    rev for box launch

--- _API/app.py
+++ _API/app.py
@@ -6,10 +6,10 @@ app = Flask(__name__)
 CORS(app)
 
 import logging
-import nlc
-import response
+# import nlc
+from response import *
 import reboot
-import monitor
+# import monitor
 
 logger = logging.getLogger()
 
@@ -27,8 +27,7 @@ def service():
     d = dict()
 
     try :
-
-        start = response.start_time()
+        start = start_time()
         payload = request.json
         command = payload['cmd']
         param = payload['param']
@@ -38,23 +37,23 @@ def service():
 
             if param >= 1:
                 nlc.door(param)
-                end = response.end_time()
-                r["latency"] = response.latency(start,end)
+                end = end_time()
+                r["latency"] = latency(start,end)
                 r["param"] = param
                 return jsonify(r)
 
             else:
-                end = response.end_time()
+                end = end_time()
                 r["code"] = "101"
                 r["message"] = "Parameter is not valid."
-                r["latency"] = response.latency(start,end)
+                r["latency"] = latency(start,end)
                 return jsonify(r)
 
         elif command == 'doorstat':
             if param >= 1:
                 d = nlc.doorStatus(param)
-                end = response.end_time()
-                r["latency"] = response.latency(start,end)
+                end = end_time()
+                r["latency"] = latency(start,end)
                 response = {
                     "response" : r,
                     "data": d
@@ -64,25 +63,24 @@ def service():
             else:
                 r["code"] = "102"
                 r["message"] = "Parameter is not valid."
-                r["latency"] = response.latency(start,end)
+                r["latency"] = latency(start,end)
                 return jsonify(r)
-                
 
-        elif command == 'machinestat':
-            d = monitor.check_monitor()
-            end = response.end_time()
-            r["latency"] = response.latency(start,end)
-            r["param"] = param
-            resp = {
-                "response" : r,
-                "data": d
-                }
-            return jsonify(resp)
+        # elif command == 'machinestat':
+        #     d = monitor.check_monitor()
+        #     end = response.end_time()
+        #     r["latency"] = response.latency(start,end)
+        #     r["param"] = param
+        #     resp = {
+        #         "response" : r,
+        #         "data": d
+        #         }
+        #     return jsonify(resp)
         
         elif command == 'reboot':
             reboot.restart()
-            end = response.end_time()
-            r["latency"] = response.latency(start,end)
+            end = end_time()
+            r["latency"] = latency(start,end)
             r["param"] = param
             return jsonify(r)
         
@@ -91,8 +89,8 @@ def service():
     except :
         r["code"] = "400"
         r["message"] = "Missing Object."
-        end = response.end_time()
-        r["latency"] = response.latency(start,end)
+        end = end_time()
+        r["latency"] = latency(start,end)
         return jsonify(r)
         
 if __name__ == '__main__':
